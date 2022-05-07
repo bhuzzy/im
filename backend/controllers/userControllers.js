849948d1ec08dc5683f9da0a8dcca455be94bed2
blake hudson <bhuzschs@gmail.com>
2022-05-07 11:11:04 -0400

    from desktop

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,14 +1,55 @@
-const registerUser = (req, res) => {
-    console.log(req.body)
-    
-    res.send('Register Route')
-}
+const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
 
-const loginUser = (req, res) => {
-    res.send('Login Route')
-}
+const User = require('../models/userModel');
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  // validation
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('Please include all fields');
+  }
+
+  // find if user already exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  // hash password
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      //token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new error('Invalid user data');
+  }
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+  res.send('Login Route');
+});
 
 module.exports = {
-    registerUser,
-    loginUser,
-}
+  registerUser,
+  loginUser,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,25 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
-const PORT = process.env.PORT || 8000
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+const PORT = process.env.PORT || 8000;
 
-const app = express()
+// connect to db
+connectDB();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-    res.status(201).json({ message: 'welcome to support'})
-})
+  res.status(201).json({ message: 'welcome to support' });
+});
 
 //routes
-app.use('/api/users', require('./routes/userRoutes'))
+app.use('/api/users', require('./routes/userRoutes'));
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.use(errorHandler);
 
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
