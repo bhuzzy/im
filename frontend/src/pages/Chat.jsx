f354d8673371bd885f53b48025444dbf5b7feab1
blake hudson <bhuzschs@gmail.com>
2022-07-14 16:15:37 -0400

    added all chat functions

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,21 +1,33 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const API_URL = 'api/users/chat';
+const API_URL = 'api/chat';
 
 function Chat() {
   const [users, setUsers] = useState([]);
 
+  const token = JSON.parse(localStorage.getItem('user'));
+  //console.log(JSON.parse(localStorage.getItem('user')));
+
+  const usertoken = token.token;
+  console.log(usertoken);
+
+  const config = {
+    headers: {
+      Authorization: `Bearer ${usertoken}`,
+    },
+  };
+
   useEffect(() => {
-    axios.get(API_URL).then((response) => {
+    axios.get(API_URL, config).then((response) => {
       setUsers(response.data);
     });
-  }, []);
+  }, [config]);
   return (
     <>
       <p> hello </p>
       {users.map((user) => (
-        <li key={user._id}>{user.name}</li>
+        <li key={user._id}>{users.name}</li>
       ))}
     </>
   );
