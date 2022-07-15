11d7857efcedc20a5c6bbf3b11e52d26e2076f79
blake hudson <bhuzschs@gmail.com>
2022-07-14 20:40:05 -0400

    got chatnames listed on left

--- frontend/src/pages/Chatpage.jsx
+++ frontend/src/pages/Chatpage.jsx
@@ -7,13 +7,19 @@ function Chatpage() {
   useEffect(() => {
     axios.get('api/chat/all').then((response) => {
       setChats(response.data);
+      console.log(response.data);
     });
   }, []);
 
   return (
     <>
       {chats.map((chat) => {
-        return <li key={chat._id}>{chat.chatName}</li>;
+        return (
+          <div className='left' key={chat._id}>
+            {chat.latestMessage.sender.name} <br></br>
+            {chat.latestMessage.content}
+          </div>
+        );
       })}
     </>
   );
