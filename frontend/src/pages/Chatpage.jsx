8b2116269f7cf559ba8dd2c1de96a4c3e23a0f6f
blake hudson <bhuzschs@gmail.com>
2022-07-14 21:46:15 -0400

    got chatnames and messages

--- frontend/src/pages/Chatpage.jsx
+++ frontend/src/pages/Chatpage.jsx
@@ -2,24 +2,51 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
 function Chatpage() {
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  const token = user.token;
+
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
   const [chats, setChats] = useState([]);
+
+  const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     axios.get('api/chat/all').then((response) => {
       setChats(response.data);
-      console.log(response.data);
+      //console.log(response.data);
     });
   }, []);
+
+  useEffect(() => {});
+
+  const getMessages = async (id) => {
+    const { data } = await axios.get(`api/message/${id}`, config);
+    console.log(data);
+    setMessages(data);
+  };
 
   return (
     <>
       {chats.map((chat) => {
         return (
-          <div className='left' key={chat._id}>
+          <div
+            className='left'
+            onClick={() => getMessages(chat._id)}
+            key={chat._id}
+          >
             {chat.latestMessage.sender.name} <br></br>
             {chat.latestMessage.content}
           </div>
         );
+      })}
+      {messages.map((message) => {
+        return <div key={message._id}>{message.content}</div>;
       })}
     </>
   );
